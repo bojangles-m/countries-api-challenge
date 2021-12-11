0ebae8e86d3ce613beaef3a62881d4094a564f55
Bojan Mazej <bojan.mazej@i22.de>
2021-12-11 16:53:22 +0100

    feat: designed detail page

--- pages/[cid].tsx
+++ pages/[cid].tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import Layout from '../components/organisms/Layout';
 import Button from '../components/atoms/Button';
 import { fetchCountry } from '../useCases/fetchCountry';
 import { Country } from '../useCases/types';
 import { ArrowBackOutline } from 'react-ionicons';
+
+import styles from './Detail.module.scss';
 
 export default function Detail() {
   const router = useRouter();
@@ -17,12 +20,75 @@ export default function Detail() {
     }
   }, [router]);
 
+  const doBorderCountriesExist =
+    country && Array.isArray(country.borders) && country.borders.length > 0;
+  const borderCountries = doBorderCountriesExist && (
+    <section className={styles.section}>
+      <strong>Border Countries:</strong>
+      {country.borders.map(border => border).join(', ')}
+    </section>
+  );
   return (
     <Layout>
-      <Button to={router.back} Icon={ArrowBackOutline}>
+      <Button
+        to={router.back}
+        Icon={ArrowBackOutline}
+        customClass={styles.button}>
         Back
       </Button>
-      <h1>Detail Page</h1>
+      {country && (
+        <article className={styles.article}>
+          <div className={styles.image}>
+            <Image
+              src={country.flag}
+              height={500}
+              width={800}
+              layout="intrinsic"
+              alt={country.name}
+            />
+          </div>
+          <div className={styles.wrapper}>
+            <h2>{country.name}</h2>
+            <div className={styles.content}>
+              <section className={styles.section}>
+                <ul>
+                  <li>
+                    <strong>Native Name:</strong> {country.nativeName}
+                  </li>
+                  <li>
+                    <strong>Population:</strong>{' '}
+                    {Intl.NumberFormat().format(country.population)}
+                  </li>
+                  <li>
+                    <strong>Region:</strong> {country.region}
+                  </li>
+                  <li>
+                    <strong>Sub Region:</strong> {country.subregion}
+                  </li>
+                  <li>
+                    <strong>Capital:</strong> {country.capital}
+                  </li>
+                </ul>
+              </section>
+              <section className={styles.section}>
+                <ul>
+                  <li>
+                    <strong>Top Level Domain:</strong> {country.topLevelDomain}
+                  </li>
+                  <li>
+                    <strong>Currencies:</strong> {country.currencies}
+                  </li>
+                  <li>
+                    <strong>Languages:</strong>{' '}
+                    {country.languages.map(lang => lang.name).join(', ')}
+                  </li>
+                </ul>
+              </section>
+              {borderCountries}
+            </div>
+          </div>
+        </article>
+      )}
     </Layout>
   );
 }
